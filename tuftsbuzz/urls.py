51da212c4b81975df8ba3479e6732151f045ac21
wreckoner <dibyendudas.1990@gmail.com>
2015-05-28 11:33:08 -0400

    Updated README.md

--- tuftsbuzz/urls.py
+++ tuftsbuzz/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'home.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('home.urls')),
-    url(r'^wordpress/', include('wordpress.urls')),
+    # url(r'^$', include('home.urls')),
+    url(r'^$', include('wordpress.urls')),
     url(r'^rssfeed/', 'rssfeed.views.api'),
     url(r'^admin/', include(admin.site.urls)),
 )
